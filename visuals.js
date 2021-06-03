81e8dd5fa2301ce87ab64b4efcd3ad17fa0b1812
miholjcicmarko <u0984549@utah.edu>
2021-06-03 11:41:09 -0600

    update

--- visuals.js
+++ visuals.js
@@ -25,16 +25,64 @@ class visuals {
             this.amountList.push(i);
         }
 
-        this.drawYearBar(updateNumber);
+        this.drawYearBar();
 
     }
 
-    drawYearBar (updateNumber) {
+    drawYearBar () {
+        let that = this;
+        this.slider = false;
 
+        let amountScale = d3.scaleLinear()
+                            .domain([0, 999])
+                            .range([90, 200]);
+        
+        let amountSlider = d3.select('#slider')
+            .append('div').classed('slider-wrap', true)
+            .append('input').classed('slider', true)
+            .attr('type', 'range')
+            .attr('min', 0)
+            .attr('max', 999)
+            .attr('value', this.activeNumber);
+
+        let sliderLabel = d3.select('.slider-wrap')
+            .append('div').classed('slider-label', true)
+            .append('svg').attr("id", "slider-text");
+
+        if (this.activeNumber !== null || this.reset === true || this.activeNumber === null) {
+        let sliderText = sliderLabel.append('text')
+            .text(this.activeNumber);
+
+            sliderText.attr('x', amountScale(this.activeNumber));
+            sliderText.attr('y', 25);
+
+        amountSlider.on('input', function () {
+            that.slider = true;
+
+            sliderText
+                .text(this.value)
+                .attr('x', amountScale(this.value))
+                .attr('y', 25); 
+
+            that.updateNumber(this.value);
+
+        })
+        }
     }
 
     drawBars () {
         
+    }
+
+    updateChart (number) {
+        let that = this;
+
+        this.shotData = this.resetData;
+
+        d3.selectAll("#tooltip").remove();
+
+        this.drawBars(number);
+
     }
 
 }
